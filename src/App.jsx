fc8c9dd7d52509bca307eb854cd832e281c4aab6
Pjizen0143 <parabolic0143@gmail.com>
2026-03-26 19:45:43 +0700

    Clear Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import {IconA, IconB} from './components/Icon'
-import Image from './components/Image'
-import Header from './components/Header'
 import './App.css'
 
 function App() {
-
-  const imageURL = "https://cdn2.shopclues.com/images/thumbnails/79835/320/320/104787525124666394ID1006929615021796911502242942.jpg"
-
   return (
     <>
-      <Header />
-      Hello World<br />
-      <IconA />
-      <IconB />
-      {imageURL}<br />
-      <Image ImageURL={imageURL} />
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
     </>
   )
 }
